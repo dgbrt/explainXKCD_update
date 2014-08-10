8d1634df7b90aee57f7cb7738c63d272f94bc804
Davidoso <dayy@linux.ucla.edu>
2014-08-10 12:44:59 +0800

    Adding license and category to uploads.

--- xkcdBOT.pl
+++ xkcdBOT.pl
@@ -191,6 +191,7 @@ close(COMICLOG);
 # Upload the picture
 $bot->upload
 ({
+	data	=> "==License==\n{{XKCD file}}\n[[Category:Comic images]]",
     file    => "/opt/xkcd/$picture_name",
     title   => "$picture_name"
 })
